2a2f52b2ab297b5d5b198d878c217f71e6f5e841
Azad <nihatazad56@gmail.com>
2021-05-27 16:48:35 +0300

    Temel yapı oluşturuldu

---
+++ lib/core/base/base_model.dart
@@ -0,0 +1,5 @@
+
+
+abstract class BaseModel {
+  Map<String, Object> toMap();
+}
---
+++ lib/views/home/home/home_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import './home_view_model.dart';
+
+class HomeView extends HomeViewModel {
+  @override
+  Widget build(BuildContext context) {
+    // Replace this with your build function
+    return Center(child: Text('Çelebi Home Screen'));
+  }
+}
